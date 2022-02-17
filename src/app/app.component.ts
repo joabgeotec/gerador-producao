38d4e5a63c8f37fa0a2f9ae2a721ee816e94e533
Brito, Nunes Filho Joab <jbrito@minsait.com>
2022-02-17 11:46:35 -0300

    > calendario pronto;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import * as moment from 'moment';
+import {AfterViewInit, Component, OnInit, EventEmitter, Output, Renderer2, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatCalendar} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'gerador-producao';
+export class AppComponent implements OnInit {
+  title = 'Gerador de Produção';
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  daysSelected: string[] = new Array();
+  //calendarioAtendimento: any;
+  today: moment.Moment;
+
+  @ViewChild('calendar')
+  calendar: MatCalendar<moment.Moment>;
+
+  constructor(private _formBuilder: FormBuilder,
+              private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+    setTimeout(() => {
+          this.removeActiveDate();
+    }, 500);
+  }
+
+  getChangedValue(e: any)  {
+    console.log(moment(e).format('LL'))
+    this.daysSelected.push(moment(e).format('LL'));
+    this.highlightDays(this.daysSelected, e);
+    console.dir(this.calendar);
+  }
+
+  private highlightDays(days: string[], e: any) {
+    const dayElements = document.querySelectorAll(
+      'mat-calendar .mat-calendar-table .mat-calendar-body-cell'
+    );
+    Array.from(dayElements).forEach((element) => {
+      const matchingDay = days.find((d) => d === element.getAttribute('aria-label')) !== undefined;
+
+      if (matchingDay) {
+        // SE JÁ TIVER A CLASSE AVAILABLE, REMOVE A CLASSE AVAILABLE
+        console.dir(element);
+        if (moment(e).format('LL') == element.ariaLabel &&
+        element.classList[1] == 'available'){
+          for (let i = 0; i < this.daysSelected.length; i++) {
+            if(element.ariaLabel == this.daysSelected[i]){
+              delete this.daysSelected[i];
+              this.highlightDays(this.daysSelected, null);
+            }
+          }
+        } else {
+          this.renderer.addClass(element, 'available');
+          this.renderer.setAttribute(element, 'title', 'Event 1');
+        }
+
+      } else {
+        this.renderer.removeClass(element, 'available');
+        this.renderer.removeClass(element, 'active');
+        this.renderer.removeAttribute(element, 'title');
+      }
+    });
+  }
+
+  private removeActiveDate(){
+    const dayElements = document.querySelectorAll(
+      '.mat-calendar-body-active'
+    );
+      dayElements.forEach(element => {
+        console.log(element)
+        this.renderer.removeClass(element, 'mat-calendar-body-active');
+        this.renderer.removeAttribute(element, 'title');
+      });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatStepperModule} from '@angular/material/stepper';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +25,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
